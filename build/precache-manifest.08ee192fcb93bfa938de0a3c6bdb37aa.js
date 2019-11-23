6787b3b35f09bd4dd10ba4c3e6c0c7c91b60407d
willferrufino <will.ferrufino@gmail.com>
2019-11-23 17:10:42 -0600

    adding Creative Juice

---
+++ build/precache-manifest.08ee192fcb93bfa938de0a3c6bdb37aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4661e7d9c33c73ccdeb8005af01f0fc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc1380da598ac2876d3",
+    "url": "/static/css/main.5ab3b668.chunk.css"
+  },
+  {
+    "revision": "429fe787b0eb91993c1a",
+    "url": "/static/js/2.20a7abf4.chunk.js"
+  },
+  {
+    "revision": "8dc1380da598ac2876d3",
+    "url": "/static/js/main.32ffe9e8.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "a354edc41d80a3dc8cf31e1cd59565ac",
+    "url": "/static/media/hero-citrus.a354edc4.png"
+  },
+  {
+    "revision": "0baa4df5f982cd501ce8376bef9e4c91",
+    "url": "/static/media/logo.0baa4df5.png"
+  }
+]);
