6ba6ce0bc59e5ccf96db5592d8f47aaaaf89de54
willferrufino <will.ferrufino@gmail.com>
2019-11-24 13:51:45 -0600

    Updates

---
+++ precache-manifest.2dbd1c32552461d182c23020139a15b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7ed76900eda9f2bd6a816d44544392",
+    "url": "/willferrufino/index.html"
+  },
+  {
+    "revision": "52fc2f1f0cb4e75af126",
+    "url": "/willferrufino/static/css/main.0aec63f5.chunk.css"
+  },
+  {
+    "revision": "429fe787b0eb91993c1a",
+    "url": "/willferrufino/static/js/2.20a7abf4.chunk.js"
+  },
+  {
+    "revision": "52fc2f1f0cb4e75af126",
+    "url": "/willferrufino/static/js/main.13bd6934.chunk.js"
+  },
+  {
+    "revision": "d28a7b47ab4a45b9d134",
+    "url": "/willferrufino/static/js/runtime-main.c08a2bbe.js"
+  },
+  {
+    "revision": "a354edc41d80a3dc8cf31e1cd59565ac",
+    "url": "/willferrufino/static/media/hero-citrus.a354edc4.png"
+  },
+  {
+    "revision": "0baa4df5f982cd501ce8376bef9e4c91",
+    "url": "/willferrufino/static/media/logo.0baa4df5.png"
+  }
+]);
