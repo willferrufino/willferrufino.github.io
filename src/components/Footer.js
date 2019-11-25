5e994ecb47c3a8fdedc17091047adbd456e46375
willferrufino <will.ferrufino@gmail.com>
2019-11-24 18:15:58 -0600

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div>
+          <p>Website Design & Developed by Will Ferrufino.</p>
+          <p>Made in Austin, Texas</p>
+        </div>
+        <div>
+          <p>HTML5, Sass, ReactJS</p>
+        </div>
+      </footer>
+    );
+  }
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <aside className="sidebar">
+        <h5>Sidebar</h5>
+      </aside>
+    );
+  }
+}
